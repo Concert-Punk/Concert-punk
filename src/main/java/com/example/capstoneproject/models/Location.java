b4a67759a75f6b5b15e3bc90cc6313fd68f2de88
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-10-08 13:58:52 -0500

    location_model

--- src/main/java/com/example/capstoneproject/models/Location.java
+++ src/main/java/com/example/capstoneproject/models/Location.java
@@ -1,18 +1,50 @@
 package com.example.capstoneproject.models;
 
-
-import org.apache.catalina.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 
+
 @Entity
 @Table(name = "location")
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Location {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+    private String city;
+
+
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+    private String State;
+
+
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+    private String street_address;
+
+
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+    private String zipcode;
+
 }
+
+
