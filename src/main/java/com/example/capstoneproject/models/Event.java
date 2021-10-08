cadccb66f623091773365da8f2c14cdb38416edf
Oscar <oscarsantana2014@gmail.com>
2021-10-08 09:40:30 -0500

    Adding event model

--- src/main/java/com/example/capstoneproject/models/Event.java
+++ src/main/java/com/example/capstoneproject/models/Event.java
@@ -1,118 +1,63 @@
 package com.example.capstoneproject.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
-@Table(name = "events")
-public class events {
+@Table(name = "Events")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
+
     @Column(nullable = false, unique = true)
-    private int api_id;
-    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+    private Long api_id;
+
+    @Column(nullable = false, unique = false)
+    @Getter
+    @Setter
     private String title;
-    @Column(nullable = false, unique = true)
-    private String location;
-    @Column(nullable = false, unique = true)
-    private int datetime;
-    @Column(nullable = false, unique = true)
+
+    @Column(nullable = false, unique = false)
+    @Getter
+    @Setter
+    private Location location;
+
+    @Column(nullable = false, unique = false)
+    @Getter
+    @Setter
+    private Timestamp startTime;
+
+    @Column(nullable = false, unique = false)
+    @Getter
+    @Setter
+    private Timestamp endTime;
+
+    @Column(nullable = false, unique = false, columnDefinition = "TEXT")
+    @Getter
+    @Setter
     private String description;
-    @Column(nullable = false, unique = true)
-    private String genre;
 
-    @ManyToOne
-    @JoinColumn(name ="user_id")
-    private User owner;
-
-    public events() {
-    }
-
-    public events(String title, String location, String venue, int datetime, String description, String genre) {
-        this.title = title;
-        this.location = location;
-        this.venue = venue;
-        this.datetime = datetime;
-        this.description = description;
-        this.genre = genre;
-    }
-
-    public events(long id, int api_id, String title, String location, String venue, int datetime, String description, String genre) {
-        this.id = id;
-        this.api_id = api_id;
-        this.title = title;
-        this.location = location;
-        this.venue = venue;
-        this.datetime = datetime;
-        this.description = description;
-        this.genre = genre;
-    }
-
-    private String venue;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getApi_id() {
-        return api_id;
-    }
-
-    public void setApi_id(int api_id) {
-        this.api_id = api_id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getVenue() {
-        return venue;
-    }
-
-    public void setVenue(String venue) {
-        this.venue = venue;
-    }
-
-    public int getDatetime() {
-        return datetime;
-    }
-
-    public void setDatetime(int datetime) {
-        this.datetime = datetime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "event_tags",
+            joinColumns = {@JoinColumn(name = "event_id")},
+            inverseJoinColumns = {@JoinColumn(name = "tag_id")}
+    )
+    @Column(nullable = false, unique = false)
+    @Getter
+    @Setter
+    private List<Tag> tag;
 
 }
