813333c2214eb77d84fabef963049a56b59ca273
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-10-15 11:59:46 -0500

    updated

--- src/main/java/com/example/capstoneproject/controllers/LoginController.java
+++ src/main/java/com/example/capstoneproject/controllers/LoginController.java
@@ -2,16 +2,18 @@ package com.example.capstoneproject.controllers;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String landing() {
-        return "<H1>Hello Concert Punk</H1>";
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
+
 }
