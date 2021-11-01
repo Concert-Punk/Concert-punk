7751656a96c68c37b871c3d529258b5fd8ce200d
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-11-01 14:55:27 -0500

    save feature

--- src/main/java/com/example/capstoneproject/controllers/RestCoordinator.java
+++ src/main/java/com/example/capstoneproject/controllers/RestCoordinator.java
@@ -3,16 +3,20 @@ package com.example.capstoneproject.controllers;
 import com.example.capstoneproject.models.Event;
 import com.example.capstoneproject.models.User;
 import com.example.capstoneproject.repos.EventsRepository;
+import com.example.capstoneproject.repos.UsersRepository;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestCoordinator {
 
     private EventsRepository eventsDao;
+    private UsersRepository usersDao;
 
-    public RestCoordinator(EventsRepository eventsDao) {
+    public RestCoordinator(EventsRepository eventsDao, UsersRepository usersDao) {
         this.eventsDao = eventsDao;
+        this.usersDao = usersDao;
     }
 
     @PostMapping(value="/saveEvent",
