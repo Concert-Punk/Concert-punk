11dee52eec76631753ba025d858826a1cd117efa
Eli <84743591+Eliguajardo@users.noreply.github.com>
2021-11-11 15:38:39 -0600

    profile page

--- src/main/java/com/example/capstoneproject/controllers/ProfilePage.java
+++ src/main/java/com/example/capstoneproject/controllers/ProfilePage.java
@@ -1,33 +1,202 @@
 package com.example.capstoneproject.controllers;
 
+import com.example.capstoneproject.models.Comment;
+import com.example.capstoneproject.models.Event;
+import com.example.capstoneproject.models.Roles;
 import com.example.capstoneproject.models.User;
+import com.example.capstoneproject.repos.CommentsRepository;
+import com.example.capstoneproject.repos.EventsRepository;
+import com.example.capstoneproject.repos.UsersRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.List;
 
+@Controller
 public class ProfilePage {
+    private UsersRepository usersDao;
+    private PasswordEncoder passwordEncoder;
+    private EventsRepository eventsDao;
+    private CommentsRepository commentsDao;
 
-    @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
-    public class ViewProfileServlet extends HttpServlet {
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void UserController(UsersRepository usersDao, PasswordEncoder passwordEncoder, EventsRepository eventsDao, CommentsRepository commentsDao) {
+        this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
+        this.eventsDao = eventsDao;
+        this.commentsDao = commentsDao;
+    }
 
-            if (request.getSession().getAttribute("user") == null) {
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model) {
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
 
-                response.sendRedirect("/login");
-                return;
-            }
-            User user = (User) request.getSession().getAttribute("user");
-            try {
-                request.setAttribute("ads", DaoFactory.getAdsDao().getByUserId(user.getId()));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-        }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setIsActive(true);
+        user.setRole(Roles.user);
+        user.setPassword(hash);
+        usersDao.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/profile")
+    public String currentUserProfile(Model model) {
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        model.addAttribute("theCurrentUser", userInDB.getRole() == Roles.admin);
+        model.addAttribute("User", userInDB);
+
+        return "users/currentUserProfile";
+    }
+
+    @GetMapping("/viewedprofile")
+    public String viewedUserProfile(Model model)
+    {
+        User viewedUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(viewedUserSession.getId());
+        model.addAttribute("viewedUser", userInDB.getRole() == Roles.admin);
+        return "users/viewedProfile";
+    }
+
+    @GetMapping("/user/{username}/events")
+    public String showUserEvents(
+            @PathVariable String username,
+            Model model
+    ){
+
+        User userToDisplay = usersDao.findByUsername(username);
+        model.addAttribute("user",userToDisplay);
+
+        return "users/currentUserProfile";
+    }
+
+
+    @GetMapping("/users/edit")
+    public String showEditUserForm(Model model) {
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userToEdit = usersDao.getById(currentUserSession.getId());
+        model.addAttribute("userToEdit", userToEdit);
+        return "users/edit";
+    }
+
+    @PostMapping("/users/edit")
+    public String editUser(
+            @ModelAttribute User userToEdit
+    ) {
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        userInDB.setUsername(userToEdit.getUsername());
+        userInDB.setEmail(userToEdit.getEmail());
+        usersDao.save(userInDB);
+
+        return "redirect:/profile";
+
+    }
+
+    //    Route for adding comment
+    @PostMapping(value = "/addComment/{postId}")
+    public String addComment(@PathVariable Long postId, @RequestParam(name="commentText") String comment ) {
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        Event event = eventsDao.getById(postId);
+        Comment newComment = new Comment();
+        newComment.setComment_text(comment);
+        newComment.setUser(userInDB);
+        newComment.setEvent(event);
+        System.out.println(newComment);
+        commentsDao.save(newComment);
+        return "redirect:/events";
+    }
+
+
+
+
+
+
+
+
+    //    User Delete Account
+    @PostMapping("/users/delete")
+    public String deleteUser() {
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        usersDao.delete(userInDB);
+        return  "redirect:/";
+    }
+
+
+
+    @GetMapping("/profile/{id}")
+    public String viewProfiles(@PathVariable Long id, Model model){
+        User viewUser = usersDao.getById(id);
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        boolean isFollowing = userInDB.getFollowing().contains(viewUser);
+        model.addAttribute("isFollowing", isFollowing);
+        model.addAttribute("viewedUser", viewUser);
+        model.addAttribute("id",id );
+        return "users/viewedProfile";
+    }
+
+//User follow member
+
+    @PostMapping("/profile/{id}/follow")
+    public String followMember (@PathVariable Long id,Model model) {
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        User userToFollow = usersDao.getById(id);
+        List<User> following = userInDB.getFollowing();
+        following.add(userToFollow);
+        userInDB.setFollowing(following);
+        model.addAttribute("isFollowing", userInDB.getFollowing() == userToFollow);
+        usersDao.save(userInDB);
+        System.out.println("Works");
+        return "redirect:/profile/" + id;
+
+    }
+//User follow member
+
+//    @PostMapping("/profile/{id}/unfollow")
+//    public String unfollowMember (@PathVariable Long id,Model model){
+//        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User userInDB = usersDao.getById(currentUserSession.getId());
+//        User userToUnfollow = usersDao.getById(id);
+//        List<User>following = userInDB.getFollowing();
+//        following.remove(userToUnfollow);
+//       // userInDB.setFollowing(following);
+//        usersDao.save(userInDB);
+//        return "redirect:/profile/" + id;
+//
+//    }
+
+    //User follow member
+    @PostMapping("/profile/{id}/unfollow")
+    public String unfollowMember (@PathVariable Long id,Model model){
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        User userToUnfollow = usersDao.getById(id);
+        List<User>following = userInDB.getFollowing();
+        following.remove(userToUnfollow);
+        // userInDB.setFollowing(following);
+        usersDao.save(userInDB);
+        return "redirect:/profile/" + id;
+    }
+
+    //viewFollowers
+    @GetMapping("/profile/{id}/viewFollowers")
+    public String viewFollowers(Model model){
+        User currentUserSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userInDB = usersDao.getById(currentUserSession.getId());
+        List<User>following = userInDB.getFollowing();
+        model.addAttribute("following",following);
+        return "users/viewFollowers";
     }
 
 }
---
+++ src/main/java/com/example/capstoneproject/models/ProfilePage.java
@@ -0,0 +1,111 @@
+package com.example.capstoneproject.models;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="ProfilePage")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfilePage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+    private String username;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String password;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Getter
+    @Setter
+    private Roles role;
+
+    @Column(nullable = false, columnDefinition = "boolean")
+    @Getter
+    @Setter
+    private Boolean isActive;
+
+    @OneToOne
+    @Getter
+    @Setter
+    private Location location;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    @Getter
+    @Setter
+    private List<Event> events;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @Getter
+    @Setter
+    private List<Comment> comments;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "followers",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "followed_id")}
+    )
+    @Getter
+    @Setter
+
+    private List<User> following;
+
+    @ManyToMany(mappedBy = "following")
+    private List<User> followers;
+
+
+
+    @ManyToOne
+    @JoinColumn(name="event_id")
+    @Getter @Setter
+    private Event event;
+
+    @ManyToMany(mappedBy = "potentialAttendees")
+    private List<Event> potentialEvents;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @Getter
+    @Setter
+    private List<EventPhoto> eventphoto;
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @Getter
+    @Setter
+    private List<user_photo_table>  userphotos;
+
+//    @Getter
+//    @Setter
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "songs")
+//    private List<Song> songs;
+
+
+//    public User(User copy) {
+//        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+//        email = copy.email;
+//        username = copy.username;
+//        password = copy.password;
+//    }
+}
+
+
